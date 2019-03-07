86bd5c553f6099184a281d83a7adf4097f0b6902
HuangRunHua <h76eddy@icloud.com>
2019-03-07 15:07:15 +0800

    修改了dish.h里面的错误

--- 线性表/双向链表/双向链表/dlist.h
+++ 线性表/双向链表/双向链表/dlist.h
@@ -42,7 +42,8 @@ typedef struct DList_ {
 void dlist_init(DList *list, void (*destroy)(void *data));
 void dlist_destroy(DList *list);
 int dlist_ins_next(DList *list, DListElmt *element, const void *data);
-int dlist_rem_next(DList *list, DListElmt *element, void **data);
+int dlist_ins_prev(DList *list, DListElmt *element, const void *data);
+int dlist_remove(DList *list, DListElmt *element, void **data);
 #define dlist_size(list) ((list) -> size)
 
 #define dlist_head(list) ((list) -> head)
